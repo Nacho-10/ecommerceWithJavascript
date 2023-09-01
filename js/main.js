3976575c6eeb6eee0062e7fca0549d75b161732b
Nacho-10 <ignaciomusa26@gmail.com>
2023-09-01 06:11:30 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,339 @@
+const productosArray = [
+    {
+        id: 'remera-01',
+        titulo: 'Remera-01',
+        imagen: '../../image/remeras/remera1.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-02',
+        titulo: 'Remera-02',
+        imagen: '../../image/remeras/remera2.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-03',
+        titulo: 'Remera-03',
+        imagen: '../../image/remeras/remera3.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-04',
+        titulo: 'Remera-04',
+        imagen: '../../image/remeras/remera4.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-05',
+        titulo: 'Remera-05',
+        imagen: '../../image/remeras/remera5.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-06',
+        titulo: 'Remera-06',
+        imagen: '../../image/remeras/remera6.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-07',
+        titulo: 'Remera-07',
+        imagen: '../../image/remeras/remera7.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-08',
+        titulo: 'Remera-08',
+        imagen: '../../image/remeras/remera8.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-09',
+        titulo: 'Remera-09',
+        imagen: '../../image/remeras/remera9.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'remera-010',
+        titulo: 'Remera-010',
+        imagen: '../../image/remeras/remera10.jpg',
+        categoria: {
+            nombre: 'Remera',
+            id: 'remeras'
+        },
+        precio: 3000
+    },
+    {
+        id: 'pantalon-01',
+        titulo: 'Pantalon-01',
+        imagen: '../../image/pantalones/pantalon1.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-02',
+        titulo: 'Pantalon-02',
+        imagen: '../../image/pantalones/pantalon2.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-03',
+        titulo: 'Pantalon-03',
+        imagen: '../../image/pantalones/pantalon3.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-04',
+        titulo: 'Pantalon-04',
+        imagen: '../../image/pantalones/pantalon4.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-05',
+        titulo: 'Pantalon-05',
+        imagen: '../../image/pantalones/pantalon5.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-06',
+        titulo: 'Pantalon-06',
+        imagen: '../../image/pantalones/pantalon6.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-07',
+        titulo: 'Pantalon-07',
+        imagen: '../../image/pantalones/pantalon7.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-08',
+        titulo: 'Pantalon-08',
+        imagen: '../../image/pantalones/pantalon8.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-09',
+        titulo: 'Pantalon-09',
+        imagen: '../../image/pantalones/pantalon9.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'pantalon-010',
+        titulo: 'Pantalon-010',
+        imagen: '../../image/pantalones/pantalon10.jpg',
+        categoria: {
+            nombre: 'Pantalon',
+            id: 'pantalones'
+        },
+        precio: 4000
+    },
+    {
+        id: 'zapatilla-01',
+        titulo: 'Zapatilla-01',
+        imagen: '../../image/zapatillas/zapatilla1.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-02',
+        titulo: 'Zapatilla-02',
+        imagen: '../../image/zapatillas/zapatilla2.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-03',
+        titulo: 'Zapatilla-03',
+        imagen: '../../image/zapatillas/zapatilla3.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-04',
+        titulo: 'Zapatilla-04',
+        imagen: '../../image/zapatillas/zapatilla4.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-05',
+        titulo: 'Zapatilla-05',
+        imagen: '../../image/zapatillas/zapatilla5.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-06',
+        titulo: 'Zapatilla-06',
+        imagen: '../../image/zapatillas/zapatilla6.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-07',
+        titulo: 'Zapatilla-07',
+        imagen: '../../image/zapatillas/zapatilla7.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-08',
+        titulo: 'Zapatilla-08',
+        imagen: '../../image/zapatillas/zapatilla8.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-09',
+        titulo: 'Zapatilla-09',
+        imagen: '../../image/zapatillas/zapatilla9.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    },
+    {
+        id: 'zapatilla-010',
+        titulo: 'Zapatilla-010',
+        imagen: '../../image/zapatillas/zapatilla10.jpg',
+        categoria: {
+            nombre: 'Zapatilla',
+            id: 'zapatillas'
+        },
+        precio: 5000
+    }
+];
+
+const contenedorProductos= document.querySelector('#contenedor-productos');
+const botonesCategorias= document.querySelectorAll('.boton-categoria');
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML= '';
+
+    productosElegidos.forEach(producto => {
+        const div= document.createElement('div')
+        div.classList.add('producto');
+        div.innerHTML = `<img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">$${producto.precio}</p>
+            <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>`;
+
+        contenedorProductos.append(div);
+    })    
+};
+cargarProductos(productos)
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener('click', (e)=>{
+        botonesCategorias.forEach(boton => boton.classList.remove('active'));
+        e.currentTarget.classList.add('active');
+
+        if(e.currentTarget.id != 'todos'){
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id)
+            cargarProductos(productosBoton);
+        } else {
+            cargarProductos(productos)
+        }
+        
+    })
+})
